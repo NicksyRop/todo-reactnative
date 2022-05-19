3ae383dddeb26acb99f684d2f924338f6505b88a
NicksyRop <nicksonkipkorir25@gmail.com>
2022-05-19 12:25:52 +0300

    added a TodoList component

---
+++ components/TodoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const TodoList = () => {
+  return (
+    <View>
+      <Text>Todo list</Text>
+    </View>
+  );
+};
