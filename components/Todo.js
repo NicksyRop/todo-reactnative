70045953653aa698a124cedba385c9f2dfae2546
NicksyRop <nicksonkipkorir25@gmail.com>
2022-05-19 13:37:01 +0300

    added a text input for adding items

--- components/Todo.js
+++ components/Todo.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { View, Button, Text } from "react-native";
+import { View, Button, Text, StyleSheet } from "react-native";
 
 export const Todo = (props) => {
   return (
-    <View>
+    <View style={[{ margin: 8, padding: 8 }, styles.item]}>
       <Text>{props.name}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "whitesmoke",
+  },
+});
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
-import { Text, View, Button } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from "react-native";
 import { Todo } from "./Todo";
 
 export const TodoList = () => {
-  const [title, setTitle] = useState("todo list");
+  const [title, setTitle] = useState("Todo list");
 
   return (
-    <View>
-      <Text>{title}</Text>
-      <Todo name="First to do" />
-      <Todo name="Second to do" />
-      <Button title="change me" onPress={() => setTitle("My List")} />
+    <View style={{ width: "80%", marginBottom: 60 }}>
+      <Text style={[styles.align, styles.font]}>{title}</Text>
+      <ScrollView>
+        <Todo name="First to do" />
+        <Todo name="Second to do" />
+      </ScrollView>
+      <View>
+        <TextInput style={styles.input} />
+        <Button title="Add item" onPress={() => setTitle("My List")} />
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  align: {
+    alignSelf: "center",
+  },
+  font: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+  },
+});
